cfbdb8f2cecee8be2080dd02c18627e274c0b9ac
Krystal Xu <xx5xm@virginia.edu>
2013-06-18 00:35:54 -0700

    Edit models a bit

--- models.py
+++ models.py
@@ -1,26 +1,31 @@
 from google.appengine.ext import ndb
 
+
 class User(ndb.Model):
-    creatorid = ndb.StringProperty(required=True)
+    creator_id = ndb.StringProperty(required=True)
     notebook = ndb.StructuredProperty(Notebook, required=True)
-	
+
+
 class Notebook(ndb.Model):
-    ownerid = ndb.StringProperty()
-    bunnies = ndb.StructuredProperty(Bunny, repeated=true)
+    owner_id = ndb.StringProperty()
+    bunnies = ndb.StructuredProperty(Bunny, repeated=True)
+
 
 class Bunny(ndb.Model):
-lectureid = ndb.IntegerProperty(required=True) #identifier of lecture
-    bunnyid = ndb.IntegerProperty(required=True) #unique bunny id
-    creatorid = ndb.StringProperty(required=True) #identifier for creator
-    rating = ndb.IntegerProperty(required=True) #note rating
-    timestamp = ndb.DateTimeProperty(auto_now_add) #timestamp for sorting
-    attatched_bunnies = ndb.StringProperty()
+    lecture_id = ndb.IntegerProperty(required=True)     #identifier of lecture
+    bunny_id = ndb.IntegerProperty(required=True)       #unique bunny id
+    creator_id = ndb.StringProperty(required=True)      #identifier for creator
+    rating = ndb.IntegerProperty(required=True)         #note rating
+    timestamp = ndb.DateTimeProperty(auto_now_add=True) #timestamp for sorting
+    attached_bunnies = ndb.StringProperty()
     note = ndb.TextProperty(required=True)
-    
+
     @classmethod
     def get_lecture_bunnies(cls, lecture_key):
-        return cls.query(lectureid=lecture_key)
-    def get_bunny(cls, thisid):
-        return cls.query(bunnyid=thisid)
+        return cls.query(lecture_id=lecture_key)
+
+    def get_bunny(cls, this_id):
+        return cls.query(bunny_id=this_id)
+
     def get_user_bunnies_for_lecture(cls, lecture_key, user_key):
-        return cls.query(cls, lectureid=lecture_key, creatorid=user_key)
+        return cls.query(cls, lecture_id=lecture_key, creator_id=user_key)
