b181f08e752106bca3b6fb51b9a9d6b55b187d4c
Krystal Xu <xx5xm@virginia.edu>
2013-06-17 22:50:06 -0700

    Add templates folder + files, basic routing

---
+++ main.py
@@ -0,0 +1,40 @@
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates/"),
+    extensions=['jinja2.ext.autoescape'])
+
+def render(self, template_values, template_url):
+    template = JINJA_ENVIRONMENT.get_template(template_url)
+    self.response.write(template.render(template_values))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        render(self, {}, 'index.html')
+
+class RegistrationHandler(webapp2.RequestHandler):
+    def get(self):
+        render(self, {}, 'register.html')
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        render(self, {}, 'login.html')
+
+class LogoutHandler(webapp2.RequestHandler):
+    def get(self):
+        render(self, {}, 'logout.html')
+
+class AccountPage(webapp2.RequestHandler):
+    def get(self):
+        render(self, {}, 'account.html')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/register', RegistrationHandler),
+    ('/login', LoginHandler),
+    ('/logout', LogoutHandler),
+    ('/account', AccountPage),
+], debug=True)
